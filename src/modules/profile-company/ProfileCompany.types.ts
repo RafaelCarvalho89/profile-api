718ff9de4a3b370bca3ee16587c82106d1704fa3
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2024-10-18 14:58:49 -0300

    feat: create profile company types

---
+++ src/modules/profile-company/ProfileCompany.types.ts
@@ -0,0 +1,18 @@
+import { AddressType, BaseDataType, BaseType } from '../common/Common.types';
+import { CompanyAddressDataType, CompanyDataType, CompanyType } from '../company/Company.types';
+
+export type ProfileCategoryType = 'seller' | 'buyer';
+
+export type ProfileCompanyDataType = BaseDataType & {
+  profile_category: ProfileCategoryType;
+  company: CompanyDataType;
+  address: CompanyAddressDataType;
+  termsAccepted: boolean;
+};
+
+export type ProfileCompanyType = BaseType & {
+  profileCategory: ProfileCategoryType;
+  company: CompanyType;
+  address: AddressType;
+  termsAccepted: boolean;
+};
