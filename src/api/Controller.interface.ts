ce612aa72209cca73a6280420f690c519c609187
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2024-10-18 14:40:17 -0300

    feat: express server

--- src/api/Controller.interface.ts
+++ src/api/Controller.interface.ts
@@ -1,7 +1,8 @@
+import { Connection } from '../db/MysqlConfig';
 import { RequestType, ResponseType } from './Request.types';
 
 export interface ControllerInterface {
   handle(request: RequestType): Promise<ResponseType<Record<string, any>>>;
 }
 
-export type ControllerFactoryType = (dataSource: any) => ControllerInterface;
+export type ControllerFactoryType = (dataSource: Promise<Connection>) => ControllerInterface;
---
+++ src/api/ExpressServer.ts
@@ -0,0 +1,48 @@
+import express, { Request, RequestHandler, Response, json } from 'express';
+import { ControllerInterface } from './Controller.interface';
+import { RequestMethodType } from './Request.types';
+import { RouteMapType } from './Routes';
+import { Connection } from '../db/MysqlConfig';
+
+export class ExpressServer {
+  private readonly server = express();
+
+  constructor(
+    private readonly PORT: number,
+    private readonly HOST: string,
+    private readonly routes: RouteMapType,
+    private readonly dataSource: Promise<Connection>,
+  ) {
+    this.server.use(json());
+    this.setRoutes(this.routes);
+  }
+
+  async start(): Promise<void> {
+    this.server.listen(this.PORT, this.HOST).on('listening', () => {
+      console.log(`[LOG] EXPRESS - Http server listen on http://${this.HOST}:${this.PORT}`);
+    });
+  }
+
+  private setRoutes(routes: RouteMapType): void {
+    for(const [routePath, controllerFactory] of Array.from(routes.entries())) {
+      const splitedRoutePath = routePath.split(' ');
+
+      const method = splitedRoutePath[0] as RequestMethodType;
+      const path = splitedRoutePath[1];
+
+      this.server[method](path, this.adaptRoute(controllerFactory(this.dataSource)));
+
+      console.log(`[LOG] EXPRESS - Configured Route ${method.toUpperCase()} ${path}`);
+    }
+  }
+
+  private adaptRoute(controller: ControllerInterface): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { body, headers, params, query } = req;
+
+      const response = await controller.handle({ body, headers, params, query });
+
+      res.status(response.statusCode).json(response.body);
+    };
+  }
+}
