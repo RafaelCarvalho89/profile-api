00c8c894037957fe7b56d8748b7b0da56fa3a265
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2024-10-18 16:58:23 -0300

    feat: use mysql connection class

--- src/Application.ts
+++ src/Application.ts
@@ -1,24 +1,25 @@
-import { connection, Connection } from './db/MysqlConfig';
 import { RouteMapType, routes } from './api/Routes';
 import { ExpressServer } from './api/ExpressServer';
+import { MysqlConnection } from './db/MysqlConnection';
+import { mysqlConfig } from './db/MysqlConfig';
 
 export class Application {
-  private readonly dbConnection: Promise<Connection>;
+  private readonly dbConnection;
   private readonly routes: RouteMapType;
   private readonly apiServer;
 
   constructor(host: string, port: number) {
-    this.dbConnection = connection;
+    this.dbConnection = new MysqlConnection(mysqlConfig);
     this.routes = routes;
-    this.apiServer = new ExpressServer(port, host, this.routes, this.dbConnection);
+    this.apiServer = new ExpressServer(port, host, this.routes, this.dbConnection.dataSource);
   }
 
   async start() {
     try {
-      (await this.dbConnection).connect();
+      await this.dbConnection.connect();
       await this.apiServer.start();
     } catch (error) {
-      (await this.dbConnection).end();
+      await this.dbConnection.end();
       console.error(error);
       throw error;
     }
--- src/db/MysqlConfig.ts
+++ src/db/MysqlConfig.ts
@@ -1,12 +1,12 @@
-import mysql, { Connection } from 'mysql2/promise';
+import { Connection, ConnectionOptions } from 'mysql2/promise';
 import 'dotenv/config'
 
-const connection: Promise<Connection> = mysql.createConnection({
+const mysqlConfig: ConnectionOptions = {
   host: process.env.MYSQLDB_HOST,
   port: Number(process.env.MYSQLDB_PORT),
   database: process.env.MYSQLDB_DATABASE,
   user: process.env.MYSQLDB_USER,
   password: process.env.MYSQLDB_PASSWORD,
-});
+};
 
-export { connection, Connection };
+export { mysqlConfig, Connection };
